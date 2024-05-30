bbb4f2dd48e52922dbca5828b0adaa79c16af4ff
lukasuz <lukas@uzolas.com>
2024-05-30 16:41:22 +0200

    page init

---
+++ page/static/script.js
@@ -0,0 +1,30 @@
+function initAnimation(delay = 200) {
+
+    cols = ['blue', 'green', 'yellow', 'red'];
+    counter = 0;
+
+    elements = document.getElementsByClassName('animated');
+    len = elements.length;
+    for (let i = 0; i < len; i++) {
+        h1 = elements[i];
+        h1.innerHTML = h1.innerHTML
+            .split('')
+            .map(letter => {
+              return '<span>' + letter + '</span>';
+            })
+            .join('');
+    
+        Array.from(h1.children).forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add('wavy');
+                span.style.color = cols[counter];
+                counter = (counter + 1) % 4;
+            }, index * 60 + delay);
+        });
+    }
+    
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    initAnimation();
+});
